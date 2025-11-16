17740814dced331e4fa9f091003f3e87bab5cc66
shrimp-master <sofiacatalinamv07@gmail.com>
2025-11-16 00:48:58 -0300

    pag apuntes funcional

--- contenidos/urls.py
+++ contenidos/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name='contenidos'
 
 urlpatterns = [
-    path('', views.apuntes, name='apuntes'),
-    path('<slug:slug>', views.apuntes_bov, name='b-o-v'),
+    path('', views.apuntes, name='apuntes-base'),
+    path('<slug:slug>', views.apuntes_detalle, name='detalle-base'),
 ]
